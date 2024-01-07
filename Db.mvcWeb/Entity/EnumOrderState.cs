922b968763324f89ba32e92d299024d8a3548f6b
402505@cs.ktu.edu.tr <402505@cs.ktu.edu.tr>
2024-01-07 12:14:18 +0300

    Proje dosyası ekle.

---
+++ Db.mvcWeb/Entity/EnumOrderState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Db.mvcWeb.Entity
+{
+    public enum EnumOrderState
+    {
+        [Display(Name = "Onay bekleniyor.")]
+        Waiting,
+        [Display(Name = "Kargoya verildi.")]
+        OnTheWay,
+        [Display(Name = "Tamamlandı.")]
+        Completed
+    }
+}
---
+++ Db.mvcWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Db.mvcWeb.MvcApplication" Language="C#" %>
